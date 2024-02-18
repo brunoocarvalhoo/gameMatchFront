1133c1c58500be35fcefa4f7ccfe8e7708c99b08
Bruno Carvalho <bruno.carvalho310303@gmail.com>
2024-02-18 02:39:24 -0300

    Adiciona logo no projeto e importa header no APP

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
+    <Header/>
     <img src="./assets/ogb_logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
+import Header from './components/Header.vue'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Header
+  }
 }
 </script>
 
@@ -18,6 +23,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
